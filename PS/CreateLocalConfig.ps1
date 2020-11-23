65cbbaad8cb7eca2c1a561bcf74a117d3f8d27c1
Viacheslav Iudin <viacheslav_iudin@epam.com>
2020-11-23 08:25:38 +0400

    debug25

--- PS/CreateLocalConfig.ps1
+++ PS/CreateLocalConfig.ps1
@@ -4,8 +4,8 @@ $ADName = "EPAM_DEV";
 $Domain = "epam.com";
 $SizeLimit = "300";
 
-$inFileName =  $checkoutDir + "\LetsBuildIt\LetsBuildIt\local.config.txt";
-$outFileName =  $checkoutDir + "\LetsBuildIt\LetsBuildIt\local.config";
+$inFileName =  "LetsBuildIt\LetsBuildIt\local.config.txt";
+$outFileName = "LetsBuildIt\LetsBuildIt\local.config";
 $xmlDoc = [System.Xml.XmlDocument](Get-Content $inFileName);
 $newXmlAddElement = $xmlDoc.CreateElement("add");
 $newXmlAddElement.SetAttribute("Name", $ADName);
